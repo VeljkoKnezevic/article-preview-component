0b6e54d95edc6b40817f0c9caadc3e834c292f33
Veljko <veljkoBitno@gmail.com>
2023-02-10 18:05:25 +0100

    deleted

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//socialmediashare
-//persondata
-//sharebutton
-
-const passive = document.querySelector(".person-data");
-const active = document.querySelector(".social-media-share");
-const btn = document.querySelector(".share-button");
-
-btn.addEventListener("click", () => {
-  if(passive.style.display = "flex") {
-    active.style.display = "flex";
-    passive.style.display = "none"
-  }
-
-  btn.addEventListener("click", () => {
-    if(active.style.display = "flex") {
-      active.style.display = "none";
-      passive.style.display = "flex"
-    }
-  })
-})
 
 
 
