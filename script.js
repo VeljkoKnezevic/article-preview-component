ef05ac9ae8ff50a9dd4090bf895f15b0ff5f43e3
Veljko <veljkoBitno@gmail.com>
2023-02-11 19:13:02 +0100

    Finished styling and js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const passive = document.querySelector(".main__passive");
+const active = document.querySelector(".main__active");
+const button = document.querySelector(".main__share");
 
-
-
+button.addEventListener("click", () => {
+  if (passive.style.display === "none" || passive.style.display === "") {
+    active.style.display = "none";
+    passive.style.display = "flex";
+  } else if (active.style.display === "none" || active.style.display === "") {
+    passive.style.display = "none";
+    active.style.display = "flex";
+  }
+});
